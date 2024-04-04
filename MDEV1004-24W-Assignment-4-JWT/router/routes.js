f9be7c397a32f02c05817c644546350a30caec67
amar0898 <amar.pinjore0898@gmail.com>
2024-04-03 22:25:50 -0400

    created authToken to verify the access auth token of individual user for security

--- MDEV1004-24W-Assignment-4-JWT/router/routes.js
+++ MDEV1004-24W-Assignment-4-JWT/router/routes.js
@@ -10,6 +10,7 @@ const express = require("express")
 const app = express();
 app.use(express.json());
 const bookController = require("../controller/controller");
+const authToken = require("../middleware/authToken")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const User = require("../model/userModel");
@@ -18,24 +19,24 @@ const router = express.Router();
 
 // API Endpoints to perform the CRUD operations
 // 1.API Endpoint to fetch all books 
-router.get('/getAllBooks', bookController.getAllBooks);
+router.get('/getAllBooks', bookController.getAllBooks, authToken);
 
 // 2.API Endpoint to fetch one specific book using ID
-router.get('/getBookByID/:id', bookController.getBookByID);
+router.get('/getBookByID/:id', bookController.getBookByID, authToken);
 
 // 3.API Endpoint to create a new book  
-router.post('/createBook', bookController.createBook);
+router.post('/createBook', bookController.createBook, authToken);
 
 // 4.API Endpoint to update the book by ID
-router.put('/updateBook/:id', bookController.updateBook);
+router.put('/updateBook/:id', bookController.updateBook, authToken);
 
 // 5.API Endpoint to delete a book using ID
-router.delete('/deleteBookByID/:id', bookController.deleteBookByID);
+router.delete('/deleteBookByID/:id', bookController.deleteBookByID, authToken);
 
 //register
-router.post("/register", bookController.registerUser);
+router.post("/register", bookController.registerUser, authToken);
 
 //login
-router.post("/login", bookController.loginUser);
+router.post("/login", bookController.loginUser, authToken);
 
 module.exports = router;
